cade861ec48fe6e31a3fe5510863fb4865ed4772
ZQWhiting <zach.whiting@icloud.com>
2020-11-02 18:23:04 -0700

    guestbook template

--- client/src/pages/GuestBook.js
+++ client/src/pages/GuestBook.js
@@ -1,5 +1,19 @@
+// send a message to the couple
+// attach your contact
+
 function GuestBook() {
-	return null;
+	return (
+		<div>
+			<div>
+				<h2>Send a message to the couple</h2>
+				<form></form>
+			</div>
+			<div>
+				<h2>Attach your contacts</h2>
+				<form></form>
+			</div>
+		</div>
+	);
 }
 
 export default GuestBook;
