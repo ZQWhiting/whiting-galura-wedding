6c4f9ca60382e87ef5f1743bae4693308a33720a
ZQWhiting <zach.whiting@icloud.com>
2020-11-12 17:55:01 -0700

    added monetary donations to registry

--- client/src/pages/registry.js
+++ client/src/pages/registry.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Registry() {
 	return (
 		<>
@@ -5,20 +7,33 @@ function Registry() {
 				<a
 					className='d-block h4'
 					href='https://www.target.com/gift-registry/giftgiver?registryId=1fd67dd9dd994cdaa18cc375b2f21000&type=WEDDING'
-					target='_blank' rel="noreferrer"
+					target='_blank'
+					rel='noreferrer'
 				>
-					Link to the Target registry
+					Target registry
 				</a>
 				<a
 					className='d-block h4'
 					href='https://www.amazon.com/wedding/registry/2Q081GXTQ5IPF'
-					target='_blank' rel="noreferrer"
+					target='_blank'
+					rel='noreferrer'
 				>
-					Link to the Amazon registry
+					Amazon registry
 				</a>
+				<a
+					className='d-block h4'
+					href='https://paypal.me/ZacharyWhiting'
+					target='_blank'
+					rel='noreferrer'
+				>
+					Monetary Donations - PayPal
+				</a>
+				<Link className='d-block h4' to='./contact'>
+					Monetary Donations - Zelle
+				</Link>
 			</div>
 			<h5 className='text-footer'>
-				We greatly appreciate any and all donations.
+				We greatly appreciate any and all contributions.
 			</h5>
 		</>
 	);
