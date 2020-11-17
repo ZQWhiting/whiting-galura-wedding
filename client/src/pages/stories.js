84ca6a32defd28b47cb457d2701c0c5a34b51d14
ZQWhiting <zach.whiting@icloud.com>
2020-11-16 22:39:21 -0700

    story list

--- client/src/pages/stories.js
+++ client/src/pages/stories.js
@@ -1,13 +1,48 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_STORIES } from '../utils/queries';
 
 function Stories() {
+	const { data, error, loading } = useQuery(QUERY_STORIES);
+
+	if (loading) {
+		return <div>loading...</div>;
+	}
+	if (error) {
+		return <div>error: {error}</div>;
+	}
+
+	const stories = data?.stories;
+
 	return (
 		<div>
-			<h5>
-				Your Stories is currently a work in progress and will be updated
-				as the date of the wedding is closer. Until then, please share
-				your own stories!
-			</h5>
+			<h3 className='font-eng-i'>Read Stories:</h3>
+			<div className='story-list w-70'>
+				{stories.map(
+					({
+						_id,
+						title,
+						createdAt,
+						updatedAt,
+						user: { name, detail },
+					}) => (
+						<Link
+							to={'/yourstories/stories/' + _id}
+							className='story-card'
+							key={_id}
+						>
+							<h5 className='story-card-title'>{title}</h5>
+							{/* {detail?.photo && <div>{detail?.photo}</div>} */}
+							<div className='story-card-author'>By: {name}</div>
+							{detail?.relationship && (
+								<div className='story-card-relationship'>
+									{detail?.relationship}
+								</div>
+							)}
+						</Link>
+					)
+				)}
+			</div>
 			<h4>
 				<Link to='/yourstories/createstory' className='font-eng-i'>
 					Share a Story
