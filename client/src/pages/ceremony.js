5398ed95b74292f8a36bcb7edd4c34fb07c19f82
ZQWhiting <zach.whiting@icloud.com>
2020-11-18 17:03:39 -0700

    our testimonies on ceremony page

--- client/src/pages/ceremony.js
+++ client/src/pages/ceremony.js
@@ -9,8 +9,8 @@ function Ceremony() {
 					November 24th, 2020.
 				</h4>
 				<h5 className='mb-3'>
-					Following the sealing, a ring ceremony will be
-					broadcasted at 6:15 pm.
+					Following the sealing, a ring ceremony will be broadcasted
+					at 6:15 pm.
 					<br />
 					The ceremony will be recorded and uploaded onto this page at
 					a later time.
@@ -19,35 +19,87 @@ function Ceremony() {
 					The link to the broadcast will be posted here.
 				</p>
 			</div>
-			{/* <article>
-				<h2>The Covenant of Marriage: Our Testimonies</h2>
+			<article className='article font-eng-i'>
+				<h2 className='align-center font-eng name-padding'>
+					The Covenant of Marriage: Our Testimonies
+				</h2>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Vestibulum rhoncus blandit ipsum vel rutrum. Integer
-					ullamcorper erat non nisi convallis, ac accumsan ante
-					malesuada. Sed porta hendrerit scelerisque. Proin pulvinar
-					libero sit amet tristique pharetra. Etiam vel lectus
-					commodo, dignissim quam eu, posuere massa. Pellentesque
-					malesuada ex ex, vitae ullamcorper ante interdum ut. Integer
-					est lectus, consequat in condimentum scelerisque, egestas at
-					massa. Nulla facilisi. Morbi vulputate vehicula lorem
-					faucibus varius. Nunc feugiat nunc id mi pellentesque
-					tincidunt. In dictum sem a lectus molestie consequat.
+					I’ve always known I wanted to marry in the temple. I grew up
+					singing the children’s song “I Love to See the Temple,”
+					which still has a special place in my heart. It speaks of
+					the hope of someday attending the house of the Lord, about
+					the joy of connecting with God through ordinances and
+					covenants there. The second verse in particular talks about
+					preparing to one day marry and begin an eternal family,
+					which has become a very real goal for me as I’ve matured and
+					now progressed with Zachary. The song as a whole became my
+					personal anthem when Zachary and I were first “dating” as
+					friends, and I had the privilege of preparing for and
+					receiving the endowment ordinance (preparatory to the
+					sealing ordinance).
 				</p>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Vestibulum rhoncus blandit ipsum vel rutrum. Integer
-					ullamcorper erat non nisi convallis, ac accumsan ante
-					malesuada. Sed porta hendrerit scelerisque. Proin pulvinar
-					libero sit amet tristique pharetra. Etiam vel lectus
-					commodo, dignissim quam eu, posuere massa. Pellentesque
-					malesuada ex ex, vitae ullamcorper ante interdum ut. Integer
-					est lectus, consequat in condimentum scelerisque, egestas at
-					massa. Nulla facilisi. Morbi vulputate vehicula lorem
-					faucibus varius. Nunc feugiat nunc id mi pellentesque
-					tincidunt. In dictum sem a lectus molestie consequat.
+					That was a really sacred time for me, a time when seeking
+					the Lord became a deeply integral part of my life—more than
+					ever before. Through my endowment, I came to understand my
+					relationship with Christ in more profound ways, learning and
+					growing this past year and a half as I’ve prepared for my
+					next covenant with God.
 				</p>
-			</article> */}
+				<p>
+					As I’ve approached this point of readiness, I can say in all
+					certainty that it is my Savior, Jesus Christ, who has guided
+					me every step of the way, providing for me personally and
+					showing me the joyous possibilities of joining together with
+					an eternal companion in His temple. I am so grateful for
+					every gift along the way, and ultimately for this gift of an
+					eternal marriage with my beloved Zachary.
+				</p>
+				<h5 className='align-center name-padding'>~Ashley</h5>
+				<p>
+					In my teenage years, I became very disillusioned with life.
+					Not a whole lot really made sense to me. Among mostly
+					everything else, things like society, people, happiness,
+					sorrow, pain, and love were all too abstract for me to
+					comprehend. My desires, purposes, beliefs, identities, and
+					needs were very limited and in constant decline. In short, I
+					was depressed. The only things I truly cared about were
+					being a “good kid”, and loving my future wife someday.
+					Nothing else really mattered.
+				</p>
+				<p>
+					Time passed and I broke and matured. I more or less learned
+					about all those things I lacked, but I lost hope in those
+					things that truly mattered to me, and so it was an empty
+					knowledge. I had long since been stripped of my title as the
+					“good kid”, and marriage was as far away as the stars in my
+					mind. Surely, my life would have been nothing more than a
+					lonely struggle had it not been for the Lord, the same Lord
+					whom I despised, who was always watching over me, teaching
+					me what I needed to know before I met my faithful wife.
+				</p>
+				<p>
+					And then, when I did meet her, and when I decided to pursue
+					her, everything changed. I found the missing pieces of my
+					soul, and it all came together in a single being, perfect
+					and all encompassing, the Lord Jesus Christ and our Heavenly
+					Father, who are separate and One.
+				</p>
+				<p>
+					I have given to them everything I know how, and I continue
+					to give everything I know how. Ashley and I, both separately
+					and together, give this marriage to Them, for them to keep,
+					as an offering and as a witness, that all things are One and
+					all things belong to Them.
+				</p>
+				<p>
+					No matter what happens from here on, it will be okay. We are
+					loved by One who watches us, who knows us, and we are about
+					to be given the ultimate gift of being able to love
+					eternally.
+				</p>
+				<h5 className='align-center'>~Zachary</h5>
+			</article>
 		</div>
 	);
 }
